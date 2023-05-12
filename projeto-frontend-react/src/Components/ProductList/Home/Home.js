b9a1081d92adc8f27608aa8d7cd16c538bafa3a1
leosilvapb <leo-leonardo-silva@hotmail.com>
2023-05-12 15:49:36 -0300

    [configuração]criei os compoenentes principais do projeto

---
+++ projeto-frontend-react/src/Components/ProductList/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ProductCards } from "../ProductCard/ProductCard";
+
+export const Home = () => {
+    return (
+        <>
+            <p>Home</p>
+            <ProductCards />
+        </>
+    )
+}
+
