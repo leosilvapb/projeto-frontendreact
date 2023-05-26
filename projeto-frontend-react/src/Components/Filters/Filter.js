a9fc570a087f6aaf78302fe6378bf40df28fe728
leosilvapb <leo-leonardo-silva@hotmail.com>
2023-05-25 21:28:53 -0300

    [configuração] estruturei o fluxo de dados usando props

--- projeto-frontend-react/src/Components/Filters/Filter.js
+++ projeto-frontend-react/src/Components/Filters/Filter.js
@@ -1,16 +1,42 @@
 import React from "react";
 import { FilterContainer, Input, Titulo } from "./filterStyle";
 
-export const Filter = () => {
+export const Filter = ({
+    minFilter,
+    setMinFilter,
+    maxFilter,
+    setMaxFilter,
+    searchFilter,
+    setSearchFilter
+}) => {
+    const handleMinFilter = (e) => {
+        if (e.target.value < 0) {
+            alert("Digite valores acima de zero!")
+            setMinFilter(0)
+        } else {
+            setMinFilter(e.target.value)
+        }
+    }
+    const handleMaxFilter = (e) => {
+        if (e.target.value < 0) {
+            alert("Digite valores acima de zero!")
+            setMaxFilter(0)
+        } else {
+            setMaxFilter(e.target.value)
+        }
+    }
+    const handleSearchFilter = (e) => {
+        setSearchFilter(e.target.value)
+    }
     return (
         <FilterContainer>
             <Titulo>Filtros</Titulo>
             <p>Valor mínimo:</p>
-            <Input type="number"></Input>
+            <Input type="number" value={minFilter} onChange={handleMinFilter}></Input>
             <p>Valor máximo:</p>
-            <Input type="number"></Input>
+            <Input type="number" value={maxFilter} onChange={handleMaxFilter}></Input>
             <p>Busca por nome:</p>
-            <Input type="text"></Input>
+            <Input type="text" value={searchFilter} onChange={handleSearchFilter}></Input>
         </FilterContainer>
     )
 }
